2acd7cb9c156616b8eb86effb1f5343a796158f7
lena <chayori@icloud.com>
2022-10-04 20:20:43 +0300

    Проверка квадрата

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход 2 числа и проверяет, является ли первое число квадратом другого.
+
+void CompareNumb(int num1, int num2)
+{
+    int num3 = num2 * num2;
+    if(num1 == num3)
+     Console.WriteLine($"{num1}. {num2}. Является");
+     else
+     Console.WriteLine($"{num1}. {num2}. Не является");
+}
+CompareNumb(12, 4);
