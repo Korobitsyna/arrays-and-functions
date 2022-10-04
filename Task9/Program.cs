752f173ade6b97830095aa1bd2ff610d2d517c7b
lena <chayori@icloud.com>
2022-10-04 22:03:29 +0300

    ay of week

---
+++ Task9/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Введите число от 1 до 7: ");
+string number = Console.ReadLine();
+int num = int.Parse(number); 
+while (!int.TryParse(Console.ReadLine(), out num) || num > 7 || num < 1) ;
+{
+if (num == 6 || num == 7)
+Console.WriteLine($"{num} -> Выходной");
+else
+Console.WriteLine($"{num} -> Рабочий");
+}
