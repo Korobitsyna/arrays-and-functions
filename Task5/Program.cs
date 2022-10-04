0cb37b0af9370b2a24880a4517c4c4b8e67c4e69
lena <chayori@icloud.com>
2022-10-04 20:23:33 +0300

    Проверка обоих чисел на квадрат

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -3,9 +3,10 @@
 void CompareNumb(int num1, int num2)
 {
     int num3 = num2 * num2;
-    if(num1 == num3)
+    int num4 = num1 * num1;
+    if(num1 == num3 || num2 == num4)
      Console.WriteLine($"{num1}. {num2}. Является");
      else
      Console.WriteLine($"{num1}. {num2}. Не является");
 }
-CompareNumb(12, 4);
+CompareNumb(4, 16);
