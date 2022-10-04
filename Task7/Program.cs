38389f183a00025f54ef712a97c350916dc264bb
lena <chayori@icloud.com>
2022-10-04 21:35:37 +0300

    Трехзначное число

---
+++ Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+int num = int.Parse(number);
+while (!int.TryParse(Console.ReadLine(), out num) || num > 999 || num < 100) ;
+
+{
+    string numericAsstring = num.ToString();
+    int secondDigit = int.Parse(numericAsstring[1].ToString());
+    Console.WriteLine($"Трехзначное: {num}, вторая цифра: {secondDigit}");
+}
+Console.ReadLine();
