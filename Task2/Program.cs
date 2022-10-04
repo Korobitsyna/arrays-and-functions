910a3c6094d097496197eb999d38c5e308ba4467
lena <chayori@icloud.com>
2022-10-04 19:38:20 +0300

    Трехзначное -> двухзначное

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая подбирает трехзначное число и удаляет вторую цифру.
+// 375 -> 35
+
+void AnswerToTask()
+
+{
+    Random rnd = new Random(); // генерирует случайное число
+    int result = rnd.Next(100, 1000); // число от 100 до 999
+    string numericAsstring = result.ToString();
+    int res = int.Parse(numericAsstring[0].ToString() + numericAsstring[2].ToString());
+    
+    Console.WriteLine($"Трехзначное {result}: {res}");
+    
+    }
+AnswerToTask();
