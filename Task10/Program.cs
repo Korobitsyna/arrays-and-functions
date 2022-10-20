8c3db0fc40d4a8cc5e4e79703df8e30a455a65e4
lena <chayori@icloud.com>
2022-10-20 21:25:52 +0300

    Сумма отрицательных и положительных чисел массива

---
+++ Task10/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9;9].
+Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 20, сумма отрицательных равна -20.
+*/
+
+//Инициализация массива рандомными числами от -9 до 9 длиной length
+int[] IntArray(int length)
+{
+int [] resultArray = new int[length];
+Random rnd = new Random(); // ссылочный тип в управляемой куче
+for (int i = 0; i < resultArray.Length; i++)
+{
+    resultArray[i] = rnd.Next(-9,10);
+}
+return resultArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+     Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+// получение числа с консоли
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+//получение суммы положительных чисел массиава
+int GetSummOfPositive(int [] array)
+{
+    int summ = 0;
+    for (int i =0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        summ += array[i];
+    }
+    return summ;
+}
+//получение суммы отрицательных чисел массиава
+int GetSummOfNegative(int [] array)
+{
+    int summ = 0;
+    for (int i =0; i < array.Length; i++)
+    {
+        if(array[i] < 0)
+        summ += array[i];
+    }
+    return summ;
+}
+int length = GetNumber($"Введите размерность массива");
+int[] arr = IntArray(length);
+PrintArray(arr);
+
+int positiveSum = GetSummOfPositive(arr);
+int negativeSum = GetSummOfNegative(arr);
+
+Console.WriteLine($"Сумма положительных: {positiveSum}, сумма отрицательных: {negativeSum}");
