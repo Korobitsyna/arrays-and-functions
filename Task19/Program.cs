e0c6282f73c5b1132ca4c7a51c6c08a9027ccd76
lena <chayori@icloud.com>
2022-10-26 21:29:29 +0300

    Проверка треугольника

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход три числа и проверяет,
+может ли существовать треугольник со сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+
+int GetNumberfromConsole(string message)
+{
+Console.WriteLine(message);
+int number = int.Parse(Console.ReadLine() ?? "");
+return number;
+}
+
+int numFirst = GetNumberfromConsole("Введите первое число");
+int numSecond = GetNumberfromConsole("Введите второе число");
+int numThird = GetNumberfromConsole("Введите третье число");
+
+if(numFirst < numSecond + numThird && numSecond < numFirst + numThird && numThird < numFirst + numSecond)
+{
+   Console.WriteLine($"Каждая сторона треугольника меньше суммы двух других сторон");
+}
+else
+{
+    Console.WriteLine($"Трегуольник не может существовать");
+}
