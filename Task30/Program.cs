a28fbc4e8bb38e5f98c294064f3e3ce8ec520217
lena <chayori@icloud.com>
2022-10-31 19:47:44 +0300

    найти элемент по индексу

---
+++ Task30/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] InitArray(int m, int n)
+{
+    int[,] newArray = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newArray[i, j] = rnd.Next(2, 10);
+        }
+    }
+    return newArray;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int row = GetNumber("Введите количество строк: ");
+int column = GetNumber("Введите количество столбцов: ");
+int[,] matrix = InitArray(row, column);
+PrintMatrix(matrix);
+Console.WriteLine("Введите индексы массива");
+int i = int.Parse(Console.ReadLine());
+int j = int.Parse(Console.ReadLine());
+if (i>row && j>column)
+Console.WriteLine("такого числа нет");
+else
+{
+    Console.WriteLine($"искомый элемент - {matrix[i, j]} ");
+}
