fc3c37311032c443d414added5edacef5b63ac4c
lena <chayori@icloud.com>
2022-10-31 18:27:26 +0300

    Создать двумерный массив

---
+++ Task25/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задайте двумерный массив размером m*n, заполненный случайными числами
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+7 3 8 1 
+*/
+
+
+        int GetNumber (string message)
+        { 
+            Console.WriteLine (message);
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        int[,] InitMatrix(int m, int n)
+        {
+            int[,] resultMatrix = new int[m, n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    resultMatrix[i, j] = rnd.Next(-10, 10);
+                }
+            }
+            return resultMatrix;
+        }
+
+        void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j] }");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        int m = GetNumber("Введите число m: ");
+        int n = GetNumber("Введите число n: ");
+        int[,]matrix = InitMatrix(m, n);
+        PrintMatrix(matrix);
